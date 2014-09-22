d1c8a725ff1b5cd6c43ba5073ad45caaa375280c
Andrew Little <andrew.little@alittlewebdesign.net>
2014-09-22 07:22:40 -0400

    Work on the javascript for the contact form validation.

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,12 @@
 var contactPage = {
 	init: function() {
 		submitEl = document.getElementById('emailSubmit'),
-		formEl = document.getElementById('emailForm');
+		formEl = document.getElementById('emailForm'),
+		fail = {
+			empty: false,
+			badEmail: false,
+			noMatch: false
+		};
 
 		submitEl.addEventListener('click', contactPage.resetForm, false);
 		submitEl.addEventListener('contactPage.reset', contactPage.validateForm, false);
@@ -14,7 +19,7 @@ var contactPage = {
 			element = submitEl;
 
 		if (document.createEvent) {
-			event = document.createEvent("HTMLEvents");
+			event = document.createEvent('HTMLEvents');
 			event.initEvent(eventName, true, true);
 		} else {
 			event = document.createEventObject();
@@ -42,31 +47,47 @@ var contactPage = {
 
 		textArea.className = "";
 
+		document.getElementById('form__error-empty').style.display = 'none';
+		document.getElementById('form__error-badEmail').style.display = 'none';
+		document.getElementById('form__error-noMatch').style.display = 'none';
+
 		document.activeElement.blur();
 
-		contactPage.customEventTrigger("contactPage.reset");
+		contactPage.customEventTrigger('contactPage.reset');
 	},
 
 	validateForm: function() {		
 		var inputs = document.getElementsByTagName('input'),
 			textArea = document.getElementById('message'),
-			fail = false;
+			emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/,
+			email = '';
 
 		for(var i = 0; i < inputs.length; i++) {
 			var input = inputs[i];
 
-			if(input.value === "") {
-				input.className = "error";
-				fail = true;
-			} 
+			// Check for empty
+			if(input.value === '') {
+				input.className = 'error';
+				fail.empty = true;
+			}
+
+			if(input.type === 'email') {
+				email = input.value;
+				var temp = emailRegex.test(email);
+				if(!emailRegex.test(email)) {
+					fail.badEmail = true;
+				}
+			}
+
+			// Check for valid email
 		}
 
-		if(textArea.value === "") {
-			textArea.className = "error";
-			fail = true;
+		if(textArea.value === '') {
+			textArea.className = 'error';
+			fail.empty = true;
 		}
 
-		if(fail) {
+		if(fail.empty || fail.badEmail || fail.noMatch) {
 			contactPage.customEventTrigger('contactPage.fail');
 		} else {
 			contactPage.customEventTrigger('contactPage.pass');
@@ -74,7 +95,13 @@ var contactPage = {
 	},
 
 	errorMsg: function() {
-		alert("Please fill in the marked fields.");
+		if(fail.empty) {
+			document.getElementById('form__error-empty').style.display = 'block';
+		}
+
+		if(fail.badEmail) {
+			document.getElementById('form__error-badEmail').style.display = 'block';
+		}
 	},
 
 	submit: function() {
